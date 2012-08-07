86a35145b70d5ad9442424675f08114c592bcc91
lsujstans7 <jstans7@lsu.edu>
2012-08-07 13:42:37 -0500

    looks good

--- QuickDemo/ViewController.m
+++ QuickDemo/ViewController.m
@@ -13,15 +13,49 @@
 @end
 
 @implementation ViewController
+@synthesize greenBox;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.greenBox.frame = CGRectMake(200, 200, 50, 200);
+    
+    [UIView animateWithDuration:2.0 
+                          delay:1.0 
+                        options:UIViewAnimationCurveEaseInOut 
+                     animations:^{
+                         self.greenBox.frame = CGRectMake(200, 200, 50, 200);
+                     } 
+                     completion:^(BOOL finished) {
+                         NSLog(@"Animation Finished");
+                     }
+     ];
+    
+    
+//    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+//    
+//    NSArray *colors = [NSArray arrayWithObjects:@"Red",@"Green",@"Blue", nil];
+//    
+//    [dictionary setObject:colors forKey:@"Colors"];
+//    
+//    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//    
+//    NSString *basePath = [paths lastObject];
+//    
+//    NSString *fullPath = [basePath stringByAppendingPathComponent:@"saved.plist"];
+//    
+//    [dictionary writeToFile:fullPath atomically:YES];
+//    
+//    NSDictionary *newDictionary = [NSDictionary dictionaryWithContentsOfFile:fullPath];
+//    NSLog(@"newDictionary %@", newDictionary);
+    
 }
 
 - (void)viewDidUnload
 {
+    [self setGreenBox:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
